405b89d3d983cf6d7531211418da530af7cfab97
parampreetsingh2006 <parampreetsingh2006@gmail.com>
2018-07-30 17:00:07 +0530

    Cleaning up

---
+++ js/monopoly_base/centerScoreBoardConfig.js
@@ -0,0 +1,18 @@
+ubsApp.centerScoreBoardConfig={
+    "centerScoreBoard":{
+    "templates":[
+        {
+            "templateId":13,
+            "templateType":"centerScoreBoard",
+            "bankBalanceTitle":"{{bankBalanceTitle}}",
+            "cashTitle":"{{cashTitle}}",
+            "debtTitle":"{{debtTitle}}",
+            "withdrawTitle":"{{withdrawTitle}}",
+            "transferTitle":"{{transferTitle}}",
+            "payOffTitle":"{{payOffTitle}}",
+            "playerNameLabel":"{{playerNameTitle}}",
+            "diceValueLabel":"{{diceValueTitle}}",
+        }
+    ]
+}
+}
