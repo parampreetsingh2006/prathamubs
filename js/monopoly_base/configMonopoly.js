05250a0df8a941251f79b5712f89edc38740ff7b
Ashutosh srivastava <2shrivastavaashutosh@gmail.com>
2018-08-07 12:14:56 +0530

    add splash and instruction screen

--- js/monopoly_base/configMonopoly.js
+++ js/monopoly_base/configMonopoly.js
@@ -20,6 +20,32 @@ monopoly.pages = {
             "style": "position:absolute;padding:10px;border-radius:5px;top:80%;left:35%;margin-left:5%; "
         }
     ],
+ 
+"Splash":[{
+    "templateId":1,
+    "templateType":"static",
+    "style": "padding:8px;text-align:center; color:white;border-radius:5px;position:absolute;top:0%;width:100%;left:0%;height:6%; background-color:black;",
+    "src":"<div id=\"splash\">	<link rel=\"stylesheet\" type=\"text/css\" href=\"css/splash.css\"		media=\"screen\" />	<script src=\"splash.js\"></script>	<!--Center align Splash contents in all screen sizes-->	<div id=\"ubs_logo\" class=\"split left\">		<div class=\"centered\">			<img src=\"images/prathamlogo.jpg\" alt=\"app_icon\"				style=\"width: 150px; height: 150px\" />	</div>	</div>	<div id=\"pratham_logo\" class=\"split right\">		<div class=\"centered\">			<img src=\"images/ubs_logo.png\" alt=\"app_icon\"				style=\"width: 150px; height: 150px\" /></div>	</div></div>"
+}
+	
+],
+
+"WelcomePage":[
+
+    {
+        "templateId":2,
+        "templateType":"static",
+        "src":"<button onclick=\"monopoly.readInstruction()\" style=\"color:white;background-color:orange;border:0;padding:5px;\">{{ReadInstructions}}</button>",
+        "style": "position:absolute;padding:10px;border-radius:5px;top:50%;left:15%;margin-left:5%; "
+    },
+    {
+        "templateId":2,
+        "templateType":"static",
+        "src":"<button onclick=\"monopoly.startGame()\" style=\"color:white;background-color:orange;border:0;padding:5px;\">{{StartGame}}</button>",
+        "style": "position:absolute;padding:10px;border-radius:5px;top:50%;right:15%;margin-right:5%; "
+    }
+],
+
 "InitialisePlayers":[{
         "templateId":1,
         "templateType":"static",
