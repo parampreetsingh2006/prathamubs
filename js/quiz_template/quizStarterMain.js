37e040b8d413e4bec3227ff779f6f59c36c523d7
parampreetsingh2006 <parampreetsingh2006@gmail.com>
2018-09-12 11:43:38 +0530

    Quiz close

--- js/quiz_template/quizStarterMain.js
+++ js/quiz_template/quizStarterMain.js
@@ -31,12 +31,7 @@ ubsApp.emptyQuizQuestions = function(){
 ubsApp.cancelQuiz=function(){
 	choiceSelected = {};
 	$("#quizCancel").attr("disabled", false);
-	$("#quizDone").attr("disabled", true);
-	$('#rollIt').attr('disabled',false);
-	$('#monopolyBase').css("z-index",0)
-	$('#templateBase').css("z-index",0)
-	$('#templateContent').css("height",0+'px')
-	document.getElementById("templateContent").innerHTML="";
+	ubsApp.closeCurrentScenario();
 }
 
 ubsApp.generalQuiz=function(config){   //credits can be amount or points depending on type of quiz
