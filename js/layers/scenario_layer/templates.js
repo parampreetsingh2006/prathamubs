8564a912213b0016c06ca23a973df7b15cf09074
parampreetsingh2006 <parampreetsingh2006@gmail.com>
2018-11-19 14:01:24 +0530

    Advantage card font size fix

--- js/layers/scenario_layer/templates.js
+++ js/layers/scenario_layer/templates.js
@@ -1154,7 +1154,7 @@ ubsApp.advantageCardTemplate='<div style="{{style}}">'+
                            '        <div style="padding-bottom:10px;"> <img src="images/red header.png" style="width:100%;">  </div>'+
                            '        '+
                            '        <div style="display: inline-block;width: 100%;font-weight:bold;">'+
-                           '            <div style="float: left;width: 50%;     margin-left: 10%; font-size: large;">'+
+                           '            <div style="float: left;width: 50%;     margin-left: 10%;">'+
                            '                <div style="display: inline-block;width: 100%;">'+
                            '                        <div style="float: left;">{{ReputationPoints}}</div>'+
                            '                        <div style="float: right;">{{currentWeekReputationPts}}</div>'+
@@ -1173,8 +1173,8 @@ ubsApp.advantageCardTemplate='<div style="{{style}}">'+
                            ''+
                            ''+
                            '        <div style="display:inline-block;margin-top: 10px;width:100%;">'+
-                           '                 <div style="float: left;margin-left: 35%;margin-right: 11px;cursor:pointer; background-image: url(images/buttonMedium.png);background-size: 100% 100%;width: fit-content;    padding: 1%; padding-bottom: 2%; color: green;font-weight:bold; width: 16%;text-align: center;font-size: large;" onclick="ubsApp.covertReputationToWildCard()" >{{OK}}</div>'+
-                           '                 <div style="cursor:pointer; float: left; background-image: url(images/buttonMedium.png);background-size: 100% 100%;width: fit-content;margin: auto;  padding: 1%; padding-bottom: 2%; color: red;font-weight: bold;font-size: large; width: 15.5%;text-align: center; padding-right: 16%; padding-left: 2%;" onclick="ubsApp.closeCurrentScenario()" >{{CANCEL}}</div>'+
+                           '                 <div style="float: left;margin-left: 35%;margin-right: 11px;cursor:pointer; background-image: url(images/buttonMedium.png);background-size: 100% 100%;width: fit-content;    padding: 1%; padding-bottom: 2%; color: green;font-weight:bold; width: 16%;text-align: center;" onclick="ubsApp.covertReputationToWildCard()" >{{OK}}</div>'+
+                           '                 <div style="cursor:pointer; float: left; background-image: url(images/buttonMedium.png);background-size: 100% 100%;width: fit-content;margin: auto;  padding: 1%; padding-bottom: 2%; color: red;font-weight: bold; width: 15.5%;text-align: center; padding-right: 16%; padding-left: 2%;" onclick="ubsApp.closeCurrentScenario()" >{{CANCEL}}</div>'+
                            '                 <div style="cursor: pointer;     float: right;padding: 3%;width: 13%;    background-size: 100% 100%;background-image: url(images/help-button.png);"  onclick="ubsApp.startHelp(\'salesHelp\')" ></div>'+
 
                            '        </div>'+
