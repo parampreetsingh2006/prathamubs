3f738d0090ae00b8c7de8511b88396e8707c6417
parampreetsingh2006 <parampreetsingh2006@gmail.com>
2018-11-19 14:04:56 +0530

    Advantage card font size fix

--- js/layers/scenario_layer/templates.js
+++ js/layers/scenario_layer/templates.js
@@ -1172,7 +1172,7 @@ ubsApp.advantageCardTemplate='<div style="{{style}}">'+
                            '        </div>'+
                            ''+
                            ''+
-                           '        <div style="display:inline-block;margin-top: 10px;width:100%;">'+
+                           '        <div style="display:inline-block;width:100%;">'+
                            '                 <div style="float: left;margin-left: 35%;margin-right: 11px;cursor:pointer; background-image: url(images/buttonMedium.png);background-size: 100% 100%;width: fit-content;    padding: 1%; padding-bottom: 2%; color: green;font-weight:bold; width: 16%;text-align: center;" onclick="ubsApp.covertReputationToWildCard()" >{{OK}}</div>'+
                            '                 <div style="cursor:pointer; float: left; background-image: url(images/buttonMedium.png);background-size: 100% 100%;width: fit-content;margin: auto;  padding: 1%; padding-bottom: 2%; color: red;font-weight: bold; width: 15.5%;text-align: center; padding-right: 16%; padding-left: 2%;" onclick="ubsApp.closeCurrentScenario()" >{{CANCEL}}</div>'+
                            '                 <div style="cursor: pointer;     float: right;padding: 3%;width: 13%;    background-size: 100% 100%;background-image: url(images/help-button.png);"  onclick="ubsApp.startHelp(\'salesHelp\')" ></div>'+
