5924567d21c290fe1cad824d880e667b4df71136
parampreetsingh2006 <parampreetsingh2006@gmail.com>
2019-02-05 19:54:04 +0530

    Add player fix

--- js/templates/add_player_template/addPlayer.js
+++ js/templates/add_player_template/addPlayer.js
@@ -45,10 +45,32 @@ ubsApp.addNewPlayer = function() {
                        "headerStyle" : "text-align: center;  color: black; font-weight: 700; "
                        });
      if(ubsApp.isAndroidEnabled) {
-        ubsApp.studentArray = JSON.parse(Android.getStudentList());
+     try {
+              ubsApp.studentArray = JSON.parse(Android.getStudentList());
+
+     } catch(err) {
+         console.log("Erro parsing student array from andriod");
+       ubsApp.studentArray=[];
+     }
      } else {
         ubsApp.studentArray = JSON.parse("[{\r\n\t\"StudentId\": \"STU111451\",\r\n\t\"StudentName\": \"JITENDRA new RAMSAJIVAN\"\r\n}, {\r\n\t\"StudentId\": \"STU111453\",\r\n\t\"StudentName\": \"ANUSHKA AMIT TIVARI\"\r\n}, {\r\n\t\"StudentId\": \"STU111448\",\r\n\t\"StudentName\": \"ANUBHAV SANTOSH\"\r\n}]");
 
      }
+
+    let numberOfPlayers = 4;
+    if(ubsApp.studentArray.length < 4) {
+        numberOfPlayers = ubsApp.studentArray.length;
+    }
+
+$('#num_online_players')
+    .find('option')
+    .remove()
+    .end();
+    for(let i=1; i<=numberOfPlayers; i++) {
+        $('#num_online_players').append('<option value="' + i + '" id="player'+ i + '">' + i + '</option>')
+    }
+
+    $('#num_online_players').val(numberOfPlayers)
+
     monopoly.initOnlinePlayers();
 }
